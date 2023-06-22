b7522063991e928c96c8c8f0e24056bcd816f4dc
Luca Cras <luca_cras@hotmail.com>
2023-06-22 19:42:44 +0200

    fix version import and uncomment main function

--- lib2/version_util.py
+++ lib2/version_util.py
@@ -1,9 +1,9 @@
 from lib2 import __version__
 
 class VersionUtil():
-    def get_version(self):
+    @staticmethod
+    def get_version():
         return __version__
 
-# if __name__ == "__main__":
-#     version_util = VersionUtil()
-#     print(f"lib2 version: {version_util.get_version()}")
+if __name__ == "__main__":
+    print(f"lib2 version: {VersionUtil.get_version()}")
